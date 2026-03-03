9cdee0873ab471e8f3f7ceabcaeedf09066c86d4
derna-doug-hash <douglaseesley@gmail.com>
2026-03-03 16:45:38 -0500

    Update MainActivity.java

--- app/src/main/java/com/autel/gimbalpro/MainActivity.java
+++ app/src/main/java/com/autel/gimbalpro/MainActivity.java
@@ -2,17 +2,32 @@ package com.autel.gimbalpro;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-// import com.autel.sdk.GimbalManager; // Temporarily disabled to allow build
+import com.autel.sdk.Autel;
+import com.autel.sdk.product.BaseProduct;
+import com.autel.sdk.ProductConnectListener;
 
 public class MainActivity extends AppCompatActivity {
-    // private GimbalManager gimbalManager; // Temporarily disabled
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // This is a placeholder to verify the build works!
-        setTitle("Autel Gimbal Pro - Loaded");
+        // This wakes up the Autel SDK
+        Autel.init(this, false);
+
+        // This waits for you to plug in the gimbal
+        Autel.setProductConnectListener(new ProductConnectListener() {
+            @Override
+            public void productConnected(BaseProduct product) {
+                // When it connects, the top bar will change
+                runOnUiThread(() -> setTitle("Gimbal Connected!"));
+            }
+
+            @Override
+            public void productDisconnected() {
+                runOnUiThread(() -> setTitle("Gimbal Disconnected"));
+            }
+        });
     }
 }
