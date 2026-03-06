f31d0e2147a51854c572a77f6d5569f173e07152
derna-doug-hash <264876854+derna-doug-hash@users.noreply.github.com>
2026-03-05 21:03:10 -0500

    Update MainActivity.java

--- app/src/main/java/com/autel/gimbalpro/MainActivity.java
+++ app/src/main/java/com/autel/gimbalpro/MainActivity.java
@@ -6,11 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.autel.sdk.Autel;
 import com.autel.sdk.AutelSdkConfig;
-import com.autel.sdk.product.BaseProduct;
 import com.autel.common.error.AutelError;
 import com.autel.common.CallbackWithNoParam;
 
-// We'll use the internal namespace for resources
+// Import the internal resource file
 import com.autel.gimbalpro.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,19 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configure the SDK for V3 hardware
+        // Configure the SDK Handshake for V3 hardware
         AutelSdkConfig config = new AutelSdkConfig.AutelSdkConfigBuilder()
                 .setPostOnUi(true)
                 .create();
 
         // Perform the Handshake (Authorization)
+        // This is the CRITICAL fix to stop the NullPointerException crash
         Autel.init(this, config, new CallbackWithNoParam() {
             @Override
             public void onSuccess() {
-                Log.d(TAG, "Autel SDK Handshake Success");
-                // Instead of a listener that might have a different name,
-                // we'll just check if the device is already there.
-                checkInitialConnection();
+                Log.d(TAG, "Autel SDK Handshake Success - Security Check Passed");
             }
 
             @Override
@@ -43,20 +40,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void checkInitialConnection() {
-        // Most stable way to check connection without specific listener imports
-        BaseProduct product = Autel.getDevice();
-        if (product != null) {
-            Log.d(TAG, "Drone Detected: " + product.getType());
-        } else {
-            Log.d(TAG, "Waiting for Drone connection...");
-        }
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Clean up the SDK when the app closes
+        // Clean up the SDK
         Autel.destroy();
     }
 }
